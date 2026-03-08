bbb3deaa981484c9cc4c284a4c731ed13a13a602
marwan ebrahim <marwanebrahim2006@gmail.com>
2026-03-08 17:36:42 +0200

    Refactor SplashScreen to streamline user navigation; replace BlocListener with direct user authentication check and navigation logic

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:movie_app/cubit/auth/user_cubit.dart';
-import 'package:movie_app/cubit/auth/user_state.dart';
 import 'package:movie_app/helpers/image_helper.dart';
 import 'package:movie_app/screens/app_layout.dart';
 import 'package:movie_app/screens/sign/login_screen.dart';
+import 'package:movie_app/services/user/user_auth_service.dart';
 import 'package:movie_app/styles/app_colors.dart';
 import 'package:movie_app/styles/app_text_styles.dart';
 
@@ -20,55 +20,59 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {});
+    _checkAndNavigate();
+  }
+
+  Future<void> _checkAndNavigate() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (!mounted) return;
+    final user = await UserAuthService().getCurrentUser();
+
+    if (!mounted) return;
+
+    if (user != null) {
+      context.read<UserAuthCubit>().isUserLogged();
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (_) => AppLayout()),
+        (route) => false,
+      );
+    } else {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (_) => BlocProvider.value(
+            value: context.read<UserAuthCubit>(),
+            child: LoginScreen(),
+          ),
+        ),
+        (route) => false,
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<UserAuthCubit, UserAuthState>(
-      listener: (context, state) {
-        if (state is UserAuthLoggedIn || state is UserAuthSuccess) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (_) => AppLayout()),
-          );
-        } else if (state is UserAuthFailure) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text(state.errorMessage)));
-        } else if (state is UserLoggedOut) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (_) => BlocProvider.value(
-                value: context.read<UserAuthCubit>(),
-                child: LoginScreen(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(ImageHelper.logoImage),
+            Text(
+              'Movie Box',
+              style: AppTextStyles.bold24.copyWith(
+                color: AppColors.whiteTextColor,
               ),
             ),
-          );
-        }
-      },
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            spacing: 8,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(ImageHelper.logoImage),
-              Text(
-                'Movie Box',
-                style: AppTextStyles.bold24.copyWith(
-                  color: AppColors.whiteTextColor,
-                ),
+            Text(
+              'Your ultimate movie discovery platform',
+              style: AppTextStyles.regular14.copyWith(
+                color: AppColors.grayTextColor,
               ),
-              Text(
-                'Your ultimate movie discovery platform',
-                style: AppTextStyles.regular14.copyWith(
-                  color: AppColors.grayTextColor,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
